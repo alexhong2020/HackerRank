438117c86944fb296322190694c9b5e32249718b
alexhong2020 <alexhong2020@gmail.com>
2021-05-28 15:54:47 -0400

    initial commit

---
+++ ProblemSolving/BeautifulTriplets/Solution.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulTriplets' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def beautifulTriplets(d, arr):
+    # Write your code here
+    s = set()
+    
+    for x in arr:
+        s.add(x)
+    
+    gc = 0
+    for x in arr:
+        if x+d in s and x+2*d in s:
+            gc+=1
+    return (gc)
+        
+        
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = beautifulTriplets(d, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
